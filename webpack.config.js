82687f3877229e204ea81308ba93eb67b3593234
David Reed <reedswork@proton.me>
2022-12-25 20:04:30 -0500

    .ts

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ const isProd = process.env.NODE_ENV === "production";
  * @type {webpack.Configuration}
  */
 const config = {
-  entry: "./src/index.tsx",
+  entry: "./src/index.ts",
   output: {
     path: fileURLToPath(new URL("./dist/", import.meta.url)),
     filename: "spadium.js",
